642751a8fdb670f5c15843075612840605112f15
Prasad Pravin Birari <prasadpbirari7@gmail.com>
2021-10-03 23:02:47 +0530

    Widgets part added

--- src/Component/Feed.jsx
+++ src/Component/Feed.jsx
@@ -15,10 +15,6 @@ export default function Feed() {
             <Post/>
             <Post/>
             <Post/>
-            <Post/>
-            <Post/>
-            <Post/>
-            <Post/>
         </div>
     )
 }
--- src/Component/Widgets.jsx
+++ src/Component/Widgets.jsx
@@ -1,9 +1,30 @@
+import { Search } from '@material-ui/icons'
 import React from 'react'
+import { TwitterShareButton, TwitterTimelineEmbed, TwitterTweetEmbed } from "react-twitter-embed"
 import "./Widgets.css"
 export default function Widgets() {
     return (
         <div className="widgets">
-            <div>Wid</div>
+            <div className="widgets_input">
+                <Search className="widget_SearchIcon" />
+                <input type="text" placeholder="Search Twitter" />
+            </div>
+
+            <div className="widgets_widgetContainer">
+                <h2>What's happening?</h2>
+                <TwitterTweetEmbed tweetId={"1444398958764232712"} />
+
+                <TwitterTimelineEmbed
+                    sourceType="profile"
+                    screenName="narendramodi"
+                    options={{ height: 400 }}
+                />
+
+                <TwitterShareButton
+                    url={"https://facebook.com/prasadpbirari"}
+                    options={{ text: "#reactjs is awesome", via: "prasadpbirari" }}
+                />
+            </div>
         </div>
     )
 }
