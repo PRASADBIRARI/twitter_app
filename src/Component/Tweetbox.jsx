46b61769064ba561919e89bf9cae1c5cd2800d62
Prasad Pravin Birari <prasadpbirari7@gmail.com>
2021-10-03 20:37:13 +0530

    Tweetbox ui completed

--- src/Component/Tweetbox.jsx
+++ src/Component/Tweetbox.jsx
@@ -6,11 +6,15 @@ export default function Tweetbox() {
         <div className="tweetBox">
             <form>
                 <div className="tweetBox__input">
-                    <Avatar src="https://miro.medium.com/fit/c/262/262/1*y0RL_nWltq-gkiJwEUjwHA.jpeg"/>
-                    <input placeholder="What's happening?" type="text"/>
-                    {/* <input placeholder="" type="text"/> */}
+                    <Avatar src="https://miro.medium.com/fit/c/262/262/1*y0RL_nWltq-gkiJwEUjwHA.jpeg" />
+                    <input placeholder="What's happening?" type="text" />
                 </div>
-                <Button>Tweet</Button>
+                <input
+                    className="tweetBox__imageInput" 
+                    placeholder="Enter image URL" 
+                    type="text"
+                />
+                <Button className="tweetBox__tweetButton">Tweet</Button>
             </form>
         </div>
     )
